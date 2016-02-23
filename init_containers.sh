1068d90563350369ba3ff8f71c646a4eb5c4d0ff
alexykot <alexykot@gmail.com>
2016-02-23 12:22:22 +0000

    - added progress echoes - fixed path for nginx conf

--- init_containers.sh
+++ init_containers.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+echo "building debian-base"
 docker build --force-rm=true -t "local:debian-base" /opt/server_provision/debian-base/
+
+echo "building letsencrypt"
 docker build --force-rm=true -t "local:letsencrypt" /opt/server_provision/letsencrypt/
+
+echo "building nginx"
 docker build --force-rm=true -t "local:nginx" /opt/server_provision/nginx/
 
+echo "running letsencrypt"
 mkdir -p /opt/server_provision/letsencrypt/tls
 docker run -d --name nginx -p 80:80 -p 443:443 -v /opt/server_provision/letsencrypt/tls:/opt/server_provision/letsencrypt/tls local:letsencrypt
 
-
+echo "running nginx"
 docker run -d --name nginx -p 80:80 -p 443:443 -v /var/www/html:/var/www/html local:nginx
 
