e20f9026940b79ea25ce6d0c5d4f690d5ee95a5b
alexykot <alexykot@gmail.com>
2016-02-23 00:38:20 +0000

    provision fix

--- provision.sh
+++ provision.sh
@@ -7,6 +7,8 @@ apt-get update
 apt-get upgrade
 apt-get install -y sudo ca-certificates mc git cpufrequtils sysfsutils apt-transport-https curl ntp
 git config --global core.editor "mcedit"
+git config --global user.email "alexykot@gmail.com"
+git config --global user.name "Alex Kotenko"
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
