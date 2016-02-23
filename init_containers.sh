28796970e9aaefceea45c53f8ad90dfbcf9d8a78
alexykot <alexykot@gmail.com>
2016-02-23 01:50:37 +0000

    nginx CMD added

--- init_containers.sh
+++ init_containers.sh
@@ -3,4 +3,4 @@
 docker build --force-rm=true -t "local:debian-base" ~/server_provision/debian-base/
 docker build --force-rm=true -t "local:nginx" ~/server_provision/nginx/
 
-docker run -d --name nginx -p 80:80 -v /var/www/html:/var/www/html local:nginx
+docker run -d --name nginx -p 80:80 -p 443:443 -v /var/www/html:/var/www/html local:nginx
