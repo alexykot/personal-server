4e0996f7305e01bcdde553b13af241206cdde56f
alexykot <alexykot@gmail.com>
2016-02-23 11:31:17 +0000

    - moved from /root to /opt

--- provision.sh
+++ provision.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #"
 
-cp -r $DIR /root/server_provision
+cp -r $DIR /opt/server_provision
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDYMKkvEMcENOkNCF1VsFe+aK3NiViEoJUt0kKgKCoOuUkLOp74jToO5kvGbcBWbvhW1e7gxKSarNdUfQ3SjdJ+FrSF8w4yYu6viNchON+x8VCSJzGR9uf99Y8qM6eemHwGzzCVNoqkx9F4r5+7hdZSz6qJpMKB0RFCg+qoyApxIxewFYGG3idSQ4CqkbO8Dazu1SYBi6dCCJhETXdakF9pMb4IBbXn0aYdIyp6Nxngpg3WLoSVIYjLPrdNTjElex1aIL0p2+O+DGuxzQWXUii5fLRN45+VFZQdHxhuAlWo+fjYUQ4qQqVdD54xtpvDOk9BaJ2ehFeKp9m5/p9BTE27 alexykot@D630" >> ~/.ssh/authorized_keys
 /etc/init.d/ssh start
@@ -20,4 +20,4 @@ apt-get update
 apt-get install -y docker-engine
 service docker start
 
-/root/server_provision/init_containers.sh
+/opt/server_provision/init_containers.sh
