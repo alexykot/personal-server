6d2323776bec6e42fd3a772a0bb28cbf3e7796b3
alexykot <alexykot@gmail.com>
2016-02-23 14:38:52 +0000

    - letsencrypt should be working now

--- init_containers.sh
+++ init_containers.sh
@@ -10,8 +10,9 @@ echo "building nginx"
 docker build --force-rm=true -t "local:nginx" /opt/server_provision/nginx/
 
 echo "running letsencrypt"
-mkdir -p /opt/server_provision/letsencrypt/tls
-docker run -d --name nginx -p 80:80 -p 443:443 -v /opt/server_provision/letsencrypt/tls:/opt/server_provision/letsencrypt/tls local:letsencrypt
+mkdir -p /opt/server_provision/letsencrypt/config
+docker run -d --name nginx -p 80:80 -p 443:443 -v /opt/server_provision/letsencrypt/config:/opt/server_provision/letsencrypt/config local:letsencrypt
+
 
 echo "running nginx"
 docker run -d --name nginx -p 80:80 -p 443:443 -v /var/www/html:/var/www/html local:nginx
